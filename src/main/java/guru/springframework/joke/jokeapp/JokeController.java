7a7cd4686f31a9f0dd90da481e9665c270bf2363
david.baur <david-baur99@web.de>
2018-12-06 10:36:03 +0100

    Marvin explained me client-specific interfaces

---
+++ src/main/java/guru/springframework/joke/jokeapp/JokeController.java
@@ -0,0 +1,12 @@
+/*package guru.springframework.joke.jokeapp;
+
+import guru.springframework.joke.jokeapp.jokes.services.JokeService;
+import guru.springframework.joke.jokeapp.jokes.services.JokeServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class JokeController {
+    @Autowired
+    private JokeService jokeService;
+} */ //explanation from Marvin
---
+++ src/main/java/guru/springframework/joke/jokeapp/jokes/services/JokeService.java
@@ -0,0 +1,6 @@
+package guru.springframework.joke.jokeapp.jokes.services;
+
+public interface JokeService {
+
+    String getJoke();
+}
